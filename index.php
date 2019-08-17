d9246b11dfbb717f6d9e8612b18d637330024728
Somov Eugene <somov.eugene.2012@gmail.com>
2019-08-17 11:23:07 +0300

    внесены изменения по присланным замечаниям

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ $show_complete_tasks = rand(0, 1);
 $projects_names = ['Входящие', 'Учеба', 'Работа', 'Домашние дела', 'Авто'];
 
 $tasks = [
-    ['name' => 'Собеседование в IT компании', 'date_completion' => '01.12.2018', 'project_name' => 'Работа', 'is_completed' => 0],
-    ['name' => 'Выполнить тестовое задание', 'date_completion' => '25.12.2018', 'project_name' => 'Работа', 'is_completed' => 0],
-    ['name' => 'Сделать задание первого раздела', 'date_completion' => '21.12.2018', 'project_name' => 'Учеба', 'is_completed' => 1],
-    ['name' => 'Встреча с другом', 'date_completion' => '22.12.2018', 'project_name' => 'Входящие', 'is_completed' => 0],
+    ['name' => 'Собеседование в IT компании', 'date_completion' => '2018-12-01', 'project_name' => 'Работа', 'is_completed' => 0],
+    ['name' => 'Выполнить тестовое задание', 'date_completion' => '2018-12-25', 'project_name' => 'Работа', 'is_completed' => 0],
+    ['name' => 'Сделать задание первого раздела', 'date_completion' => '2018-12-21', 'project_name' => 'Учеба', 'is_completed' => 1],
+    ['name' => 'Встреча с другом', 'date_completion' => '2018-12-22', 'project_name' => 'Входящие', 'is_completed' => 0],
     ['name' => 'Купить корм для кота', 'date_completion' => null, 'project_name' => 'Домашние дела', 'is_completed' => 0],
     ['name' => 'Заказать пиццу', 'date_completion' => null, 'project_name' => 'Домашние дела', 'is_completed' => 0]
 ];
@@ -34,22 +34,29 @@ function number_project_tasks(array $tasks_list, string $project_name) {
 }
 
 function hours_left_deadline($date_completion) {
-    $hours_left = 100000000;
+    if ( is_null($date_completion) ) return null;
 
-    if ( !is_null($date_completion) ) {
-        $ts_end = strtotime($date_completion);
-        $ts_now = strtotime('now');
-        $ts_diff = $ts_end - $ts_now;
-        $hours_left = floor($ts_diff / 3600);
-    }
+    $ts_end = strtotime($date_completion);
+    $ts_now = strtotime('now');
+    $ts_diff = $ts_end - $ts_now;
+    $hours_left = floor($ts_diff / 3600);
 
     return $hours_left;
 }
 
-function check_task_important(array $task) {
-    $hours_left = hours_left_deadline($task['date_completion']);
+function additional_task_classes(array $task, int $show_complete_tasks) {
+    $add_class = '';
 
-    return ($hours_left <= 24) ? "task--important" : "";
+    if ( 1 === $show_complete_tasks and 1 === $task['is_completed'] ) {
+        $add_class = 'task--completed';
+    } else {
+        $hours_left = hours_left_deadline($task['date_completion']);
+        if ( !is_null($hours_left) and $hours_left <= 24 ) {
+            $add_class = 'task--important';
+        }
+    }
+
+    return $add_class;
 }
 
 $main_content = include_template(
