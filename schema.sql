34c582e2108583b86a22be4935727d8169c1e729
Somov Eugene <somov.eugene.2012@gmail.com>
2019-08-20 22:15:59 +0300

    переименована БД, длина текстовых полей увеличена до 255 байт и изменен их тип полей на VARCHAR, для даты выполнения задачи изменен тип на DATE

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE doingsdone
+CREATE DATABASE `982685-doingsdone-10`
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-USE doingsdone;
+USE `982685-doingsdone-10`;
 
 /*
 // ---------------------------------------------
@@ -17,12 +17,12 @@ USE doingsdone;
 //   автор: пользователь, создавший проект;
 */
 CREATE TABLE projects (
-  id       	INT AUTO_INCREMENT PRIMARY KEY,
-  name     	CHAR(64) NOT NULL UNIQUE,
-  user_id  	INT NOT NULL
+  `id`       	    INT AUTO_INCREMENT PRIMARY KEY,
+  `name`     	    VARCHAR(255) NOT NULL UNIQUE,
+  `user_id`  	    INT NOT NULL
 );
 
-CREATE UNIQUE INDEX idx_projects_name ON projects (name);
+CREATE UNIQUE INDEX idx_projects_name ON projects (`name`);
 
 /*
 // = Задача =
@@ -39,18 +39,18 @@ CREATE UNIQUE INDEX idx_projects_name ON projects (name);
 //   проект: проект, которому принадлежит задача.
 */
 CREATE TABLE tasks (
-  id       	INT AUTO_INCREMENT PRIMARY KEY,
-  dt_add   	DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status   	TINYINT NOT NULL DEFAULT 0,
-  name     	CHAR(128) NOT NULL,
-  file     	CHAR(255) DEFAULT NULL,
-  dt_completion DATETIME DEFAULT NULL,
-  user_id  	INT NOT NULL,
-  project_id  	INT NOT NULL
+  `id`       	    INT AUTO_INCREMENT PRIMARY KEY,
+  `dt_add`   	    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status`   	    TINYINT NOT NULL DEFAULT 0,
+  `name`     	    VARCHAR(255) NOT NULL,
+  `file`     	    VARCHAR(255) DEFAULT NULL,
+  `dt_completion` DATE DEFAULT NULL,
+  `user_id`       INT NOT NULL,
+  `project_id`    INT NOT NULL
 );
 
-CREATE INDEX idx_tasks_name ON tasks (name);
-CREATE INDEX idx_tasks_dt_completion ON tasks (dt_completion);
+CREATE INDEX idx_tasks_name ON tasks (`name`);
+CREATE INDEX idx_tasks_dt_completion ON tasks (`dt_completion`);
 
 /*
 // = Пользователь =
@@ -63,12 +63,12 @@ CREATE INDEX idx_tasks_dt_completion ON tasks (dt_completion);
 //    пароль: хэшированный пароль пользователя.
 */
 CREATE TABLE users (
-  id       	INT AUTO_INCREMENT PRIMARY KEY,
-  dt_add   	DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  email    	CHAR(128) NOT NULL UNIQUE,
-  username 	CHAR(128) NOT NULL,
-  password 	CHAR(64) NOT NULL
+  `id`       	  INT AUTO_INCREMENT PRIMARY KEY,
+  `dt_add`   	  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `email`    	  VARCHAR(255) NOT NULL UNIQUE,
+  `username` 	  VARCHAR(255) NOT NULL,
+  `password` 	  VARCHAR(255) NOT NULL
 );
 
-CREATE UNIQUE INDEX idx_users_email ON users (email);
-CREATE INDEX idx_users_name ON users (username);
+CREATE UNIQUE INDEX idx_users_email ON users (`email`);
+CREATE INDEX idx_users_name ON users (`username`);
