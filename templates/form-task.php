30085ff2138ce166eff21492a2dd5f7fd73fd033
Somov Eugene <somov.eugene.2012@gmail.com>
2019-08-31 17:57:12 +0300

    для всех полей формы, где могут быть ошибки добавлен класс form__input--error, а также  добавлен новый тег <p> с классом form__message для вывода текста ошибки

--- templates/form-task.php
+++ templates/form-task.php
@@ -8,7 +8,7 @@
                 <a class="main-navigation__list-item-link <?= mark_active_project($project['id']) ?>" href="<?= get_link_to_project($project['id']) ?>">
                     <?= strip_tags($project['name']) ?>
                 </a>
-                <span class="main-navigation__list-item-count"><?= number_project_tasks($tasks, $project['name']); ?></span>
+                <span class="main-navigation__list-item-count"><?= $project['cnt_tasks']; ?></span>
             </li>
         <?php endforeach; ?>
         </ul>
@@ -24,32 +24,46 @@
         <div class="form__row">
             <label class="form__label" for="name">Название <sup>*</sup></label>
 
-            <input class="form__input" type="text" name="name" id="name" value="<?= get_post_value('name'); ?>" placeholder="Введите название">
+            <input class="form__input <?= isset($errors['name']) ? "form__input--error" : "" ?>" type="text" name="name" id="name"
+                value="<?= ($task['name'] ?? ''); ?>" placeholder="Введите название">
+
+            <?php if (isset($errors['name'])): ?>
+                <p class="form__message"><?= $errors['name']; ?></p>
+            <?php endif; ?>
         </div>
 
         <div class="form__row">
             <label class="form__label" for="project">Проект <sup>*</sup></label>
 
-            <select class="form__input form__input--select" name="project" id="project">
+            <select class="form__input form__input--select <?= isset($errors['project']) ? "form__input--error" : "" ?>" name="project" id="project">
             <?php foreach ($projects as $project): ?>
-                <?php $project_name = strip_tags($project['name']); ?>
-                <option value="<?= $project_name ?>"><?= $project_name ?></option>
+                <option value="<?= $project['id']; ?>" <?php if ($project['id'] === ($task['project'] ?? '')): ?>selected<?php endif; ?>>
+                    <?= strip_tags($project['name']); ?>
+                </option>
             <?php endforeach; ?>
             </select>
+
+            <?php if (isset($errors['project'])): ?>
+                <p class="form__message"><?= $errors['project']; ?></p>
+            <?php endif; ?>
         </div>
 
         <div class="form__row">
             <label class="form__label" for="date">Дата выполнения</label>
 
-            <input class="form__input form__input--date" type="text" name="date" id="date" value="<?= get_post_value('date'); ?>"
-                placeholder="Введите дату в формате ГГГГ-ММ-ДД">
+            <input class="form__input form__input--date <?= isset($errors['date']) ? "form__input--error" : "" ?>" type="text" name="date" id="date"
+                value="<?= ($task['date'] ?? ''); ?>" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
+
+            <?php if (isset($errors['date'])): ?>
+                <p class="form__message"><?= $errors['date']; ?></p>
+            <?php endif; ?>
         </div>
 
         <div class="form__row">
             <label class="form__label" for="file">Файл</label>
 
             <div class="form__input-file">
-                <input class="visually-hidden" type="file" name="file" id="file" value="<?= get_post_value('file'); ?>">
+                <input class="visually-hidden" type="file" name="file" id="file" value="<?= ($task['file'] ?? ''); ?>">
 
                 <label class="button button--transparent" for="file">
                     <span>Выберите файл</span>
