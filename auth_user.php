3c9249b76ca74a51ff40661292e831e1154aaba5
Somov Eugene <somov.eugene.2012@gmail.com>
2019-09-06 00:13:13 +0300

    в этом файле оставлен только код для получения данных о пользователе

--- auth_user.php
+++ auth_user.php
@@ -1,24 +1,4 @@
 <?php
-date_default_timezone_set('Europe/Moscow');
-setlocale(LC_ALL, 'ru-RU');
-
-require_once 'helpers.php';
-require_once 'functions.php';
-require_once 'database.php';
-
-// Mock-data
-$user_email = 'kkk@gmail.com';
-
-// подключение к MySQL
-$db_link = db_init('localhost', 'root', '', '982685-doingsdone-10');
-
-if (!$db_link) {
-    $errorMsg = 'Ошибка подключения к БД. Дальнейшая работа сайта невозможна!';
-    exit($errorMsg);
-}
-
-// ОК: cоединение установлено
-
 // получение данных аутентифицированного пользователя
 $user = get_user_by_email($db_link, $user_email);
 
@@ -28,4 +8,4 @@ if (empty($user)) {
 }
 
 $user_name = $user['username'];
-$user_id = $user['id'];
+$user_id = (integer)$user['id'];
