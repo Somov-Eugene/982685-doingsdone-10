37e2dfa83827cf032093f24c955d463436cb5e1d
Somov Eugene <somov.eugene.2012@gmail.com>
2019-09-06 00:11:34 +0300

    код для подключения к БД вынесен в отдельный файл

---
+++ db_connect.php
@@ -0,0 +1,18 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+setlocale(LC_ALL, 'ru-RU');
+
+require_once 'helpers.php';
+require_once 'functions.php';
+require_once 'database.php';
+
+// Mock-data
+$user_email = 'kkk@gmail.com';
+
+// подключение к MySQL
+$db_link = db_init('localhost', 'root', '', '982685-doingsdone-10');
+
+if (!$db_link) {
+    $errorMsg = 'Ошибка подключения к БД. Дальнейшая работа сайта невозможна!';
+    exit($errorMsg);
+}
