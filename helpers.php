c1ff8173b1c2a76b534ffdb86a304373d6f269d5
Somov Eugene <somov.eugene.2012@gmail.com>
2019-09-09 00:49:42 +0300

    исправления в функциях-помощниках (для валидации полей)

--- helpers.php
+++ helpers.php
@@ -1,108 +1,5 @@
 <?php
 /**
- * Проверяет поле в массиве $_POST и возвращает его значение, если поле существует
- *
- * @param string $name Название поля
- *
- * @return string Значение поля, если оно существует или пустую строку в противном случае
- */
-function get_post_value($name) {
-    return $_POST[$name] ?? '';
-}
-
-/**
- * Проверяет email на корректность
- *
- * @param string $name Название поля
- *
- * @return string Сообщение об ошибке, если значение поля некорректное или null, если ошибки нет
- */
-function validate_email($name) {
-    if (!filter_input(INPUT_POST, $name, FILTER_VALIDATE_EMAIL)) {
-        return "Введите корректный email";
-    }
-
-    return null;
-}
-
-/**
- * Проверяет заполненность поля
- *
- * @param string $name Название поля
- *
- * @return string Сообщение об ошибке, если значение поля некорректное или null, если ошибки нет
- */
-function validate_filled($name) {
-    if (empty($_POST[$name])) {
-        return "Это поле должно быть заполнено";
-    }
-
-    return null;
-}
-
-/**
- * Проверяет длину введенного значения
- *
- * @param string $name Название поля
- *
- * @return string Сообщение об ошибке, если значение поля некорректное или null, если ошибки нет
- */
-function validate_length($name, $min, $max) {
-    $len = strlen(get_post_value($name));
-
-    if ($len < $min or $len > $max) {
-        return "Значение должно быть от $min до $max символов";
-    }
-
-    return null;
-}
-
-/**
- * Проверяет допустимость и формат даты
- *
- * @param string $name Название поля
- *
- * @return string Сообщение об ошибке, если значение поля некорректное или null, если ошибки нет
- */
-function validate_date($name) {
-    $post_date = get_post_value($name);
-
-    // если передана какая-либо строка (дата), то выполняем проверку
-    if (!empty($post_date)) {
-        if (!is_date_valid($post_date)) {
-            return "Дата не соответствует требуемому формату или несуществующая";
-        }
-
-        $current_date = strtotime(date('Y-m-d'));
-        $checked_date = strtotime($post_date);
-
-        if ($checked_date < $current_date) {
-            return "Дата должна быть больше или равна текущей";
-        }
-    }
-
-    return null;
-}
-
-/**
- * Проверяет идентификатор выбранного проекта, что он ссылается на реально существующий проект
- *
- * @param string $name Название поля
- * @param array $allowed_list Массив с допустимыми ID
- *
- * @return string Сообщение об ошибке, если значение поля некорректное или null, если ошибки нет
- */
-function validate_project($name, $allowed_list) {
-    $id = get_post_value($name);
-
-    if (!in_array($id, $allowed_list)) {
-        return "Указан несуществующий проект";
-    }
-
-    return null;
-}
-
-/**
  * Проверяет переданную дату на соответствие формату 'ГГГГ-ММ-ДД'
  *
  * Примеры использования:
@@ -116,7 +13,8 @@ function validate_project($name, $allowed_list) {
  *
  * @return bool true при совпадении с форматом 'ГГГГ-ММ-ДД', иначе false
  */
-function is_date_valid(string $date) : bool {
+function is_date_valid(string $date) : bool
+{
     $format_to_check = 'Y-m-d';
     $dateTimeObj = date_create_from_format($format_to_check, $date);
 
@@ -145,7 +43,7 @@ function is_date_valid(string $date) : bool {
  *
  * @return string Рассчитанная форма множественнго числа
  */
-function get_noun_plural_form (int $number, string $one, string $two, string $many): string
+function get_noun_plural_form (int $number, string $one, string $two, string $many) : string
 {
     $number = (int) $number;
     $mod10 = $number % 10;
@@ -170,12 +68,15 @@ function get_noun_plural_form (int $number, string $one, string $two, string $ma
 }
 
 /**
- * Подключает шаблон, передает туда данные и возвращает итоговый HTML контент
+ * Подключает шаблон, передает туда данные и возвращает итоговый HTML-контент
+ *
  * @param string $name Путь к файлу шаблона относительно папки templates
  * @param array $data Ассоциативный массив с данными для шаблона
+ *
  * @return string Итоговый HTML
  */
-function include_template($name, array $data = []) {
+function include_template($name, array $data = []) : string
+{
     $name = 'templates/' . $name;
     $result = '';
 
@@ -190,4 +91,92 @@ function include_template($name, array $data = []) {
     $result = ob_get_clean();
 
     return $result;
+}
+
+/**
+ * Проверяет поле в массиве $_POST и возвращает его значение, если поле существует
+ *
+ * @param string $field Название поля
+ *
+ * @return string Значение поля, если оно существует или пустую строку в противном случае
+ */
+function get_post_value($field)
+{
+    return $_POST[$field] ?? '';
+}
+
+/**
+ * Проверяет email на корректность
+ *
+ * @param array $check Ассоциативный массив, в котором требуется проверить значение
+ * @param string $field Название поля (по умолчанию 'email')
+ *
+ * @return mixed Сообщение об ошибке, если значение поля некорректное или null, если ошибки нет
+ */
+function validate_email($check, $field = 'email')
+{
+    if (!filter_var($check[$field], FILTER_VALIDATE_EMAIL)) {
+        return 'Введите корректный e-mail';
+    }
+
+    return null;
+}
+
+/**
+ * Проверяет заполненность поля
+ *
+ * @param array $check Ассоциативный массив, в котором требуется проверить значение
+ * @param string $field Название поля
+ *
+ * @return mixed Сообщение об ошибке, если значение поля некорректное или null, если ошибки нет
+ */
+function validate_filled($check, $field)
+{
+    if (empty($check[$field])) {
+        return 'Это поле должно быть заполнено';
+    }
+
+    return null;
+}
+
+/**
+ * Проверяет допустимость и формат даты
+ *
+ * @param array $check Ассоциативный массив, в котором требуется проверить значение
+ * @param string $field Название поля
+ *
+ * @return mixed Сообщение об ошибке, если значение поля некорректное или null, если ошибки нет
+ */
+function validate_date($check, $field)
+{
+    if (!is_date_valid($check[$field])) {
+        return 'Дата не соответствует требуемому формату или несуществующая';
+    }
+
+    $current_date = strtotime(date('Y-m-d'));
+    $checked_date = strtotime($check[$field]);
+
+    if ($checked_date < $current_date) {
+        return 'Дата должна быть больше или равна текущей';
+    }
+
+
+    return null;
+}
+
+/**
+ * Проверяет идентификатор выбранного проекта, что он ссылается на реально существующий проект
+ *
+ * @param array $allowed_list Массив с допустимыми ID
+ * @param int $id Проверяемый ID проекта
+ *
+ * @return mixed Сообщение об ошибке, если значение поля некорректное или null, если ошибки нет
+ */
+function validate_project($allowed_list, $id)
+{
+    if (!in_array($id, $allowed_list)) {
+        return 'Указан несуществующий проект';
+    }
+
+    return null;
 }
