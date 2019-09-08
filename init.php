25a371caa2729402e67d3f1e9f0390584f557c5c
Somov Eugene <somov.eugene.2012@gmail.com>
2019-09-08 23:50:44 +0300

    инициализация

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+setlocale(LC_ALL, 'ru-RU');
+
+require_once 'db_connect.php';
+require_once 'helpers.php';
+require_once 'database_functions.php';
+require_once 'functions.php';
