df33779909388ad3e0e08c976da1cb1d56c686e9
Somov Eugene <somov.eugene.2012@gmail.com>
2019-09-09 02:02:16 +0300

    внесены изменения по присланным замечаниям

--- register.php
+++ register.php
@@ -1,16 +1,15 @@
 <?php
-require_once 'db_connect.php';
+require_once 'init.php';
 
 $page_title = "Дела в порядке - Регистрация аккаунта";
 
-// в пустую форму передаем пустые значения
+// параметры пользователя со значениями по умолчанию
 $user = [
     'email' => '',
     'password' => '',
     'name' => ''
 ];
 
-// ошибки валидации
 $errors = [];
 
 // если форма была отправлена
@@ -20,64 +19,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user['password'] = get_post_value('password');
     $user['name'] = get_post_value('name');
 
-    // обязательные к заполнению поля формы
-    $required_fields = ['email', 'password', 'name'];
-
     // правила валидации полей
     $rules = [
-        'email' => function () {
-            return validate_email('email');
+        'email' => function ($user) {
+            return validate_filled($user, 'email') ?? validate_email($user);
         },
-        'password' => function () {
-            return validate_filled('password');
+        'password' => function ($user) {
+            return validate_filled($user, 'password');
         },
-        'name' => function () {
-            return validate_filled('name');
+        'name' => function ($user) {
+            return validate_filled($user, 'name');
         }
     ];
 
-    foreach ($user as $key => $value) {
-        // для каждого поля проверяем,
-        // есть ли для этого поля правило валидации
-        if ( isset($rules[$key]) ) {
-            // получаем функцию валидации и затем вызываем ее
-            $rule = $rules[$key];
-            // возможные ошибки сохраняем в массиве $errors
-            $errors[$key] = $rule();
+    foreach ($rules as $field => $validator) {
+        if (isset($user[$field]) && is_callable($validator)) {
+            $errors[$field] = call_user_func($validator, $user, $field);
         }
     }
 
     // фильтруем массив, удаляя из него null
     $errors = array_filter($errors);
 
-    // проверка на пустое значение
-    foreach($required_fields as $field) {
-        if (empty($user[$field])) {
-            $errors[$field] = 'Это поле требуется заполнить';
-        }
-    }
-
     // если форма заполнена без ошибок, то проверяем, имеется ли переданный e-mail в базе
-    if (count($errors) === 0 and is_exist_user($db_link, $user['email'])) {
+    if (count($errors) === 0 && is_exist_user($link, $user['email'])) {
         $errors['email'] = 'Пользователь с таким e-mail уже зарегистрирован';
     }
 
     if (count($errors) === 0) {
-        // e-mail в базе не существует
-
         // получаем hash от переданного пароля
         $user['password'] = password_hash($user['password'], PASSWORD_DEFAULT);
 
         // добавляем нового пользователя в БД
-        $user_id = register_user($db_link, $user);
+        $user_id = register_user($link, $user);
 
-        if (is_null($user_id)) {
-            $errorMsg = 'Ошибка при регистрации нового пользователя';
-            exit($errorMsg);
+        if (!empty($user_id)) {
+            header("Location: index.php");
         }
-
-        // при успешном добавлении записи переадресовываем на главную страницу
-        header("Location: index.php");
     }
 }
 
