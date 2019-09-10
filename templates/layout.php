6d1697b0b4d4a90617896346a4c1da860008254d
Somov Eugene <somov.eugene.2012@gmail.com>
2019-09-11 00:25:00 +0300

    внесены изменения в лейаут - разные состояния для анонимного посетителя и для залогиненного пользователя

--- templates/layout.php
+++ templates/layout.php
@@ -9,27 +9,33 @@
     <link rel="stylesheet" href="css/flatpickr.min.css">
 </head>
 
-<body>
+<body <?= empty($user['id']) ? ' class="body-background"': ''; ?>>
 <h1 class="visually-hidden">Дела в порядке</h1>
 
 <div class="page-wrapper">
-    <div class="container container--with-sidebar">
+    <div class="container <?= empty($user['id']) ? '': 'container--with-sidebar'; ?>">
         <header class="main-header">
-            <a href="/">
+            <a href="index.php">
                 <img src="img/logo.png" width="153" height="42" alt="Логотип Дела в порядке">
             </a>
 
-            <div class="main-header__side">
-                <a class="main-header__side-item button button--plus open-modal" href="add.php">Добавить задачу</a>
+            <?php if (empty($user['id'])): ?>
+                <div class="main-header__side">
+                    <a class="main-header__side-item button button--transparent" href="auth.php">Войти</a>
+                </div>
+            <?php else: ?>
+                <div class="main-header__side">
+                    <a class="main-header__side-item button button--plus open-modal" href="add.php">Добавить задачу</a>
 
-                <div class="main-header__side-item user-menu">
-                    <div class="user-menu__data">
-                        <p><?= $user_name; ?></p>
+                    <div class="main-header__side-item user-menu">
+                       <div class="user-menu__data">
+                            <p><?= strip_tags($user['name']); ?></p>
 
-                        <a href="#">Выйти</a>
+                            <a href="logout.php">Выйти</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            <?php endif; ?>
         </header>
 
         <div class="content">
@@ -46,7 +52,9 @@
             <p>Веб-приложение для удобного ведения списка дел.</p>
         </div>
 
-        <a class="main-footer__button button button--plus" href="add.php">Добавить задачу</a>
+        <?php if (!empty($user['id'])): ?>
+            <a class="main-footer__button button button--plus" href="add.php">Добавить задачу</a>
+        <?php endif; ?>
 
         <div class="main-footer__social social">
             <span class="visually-hidden">Мы в соцсетях:</span>
