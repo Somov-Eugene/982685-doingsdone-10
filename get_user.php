88b8c1f1567b8f2e6c1ed973c392de1acb724d69
Somov Eugene <somov.eugene.2012@gmail.com>
2019-09-11 00:06:46 +0300

    даные пользователя берутся из сессии

--- get_user.php
+++ get_user.php
@@ -1,7 +1,15 @@
 <?php
-// Mock-data
-$user_email = 'kkk@gmail.com';
+session_start();
 
-// получение данных аутентифицированного пользователя
-$result = get_user_by_email($link, $user_email);
-$user = $result[0];
+// получение данных пользователя из сессии
+if (isset($_SESSION['user'])){
+    $user = $_SESSION['user'];
+} else {
+    // в сессии нет данных - анонимный пользователь
+    $user = [
+        'id' => '',
+        'name' => '',
+        'email' => '',
+        'password' => ''
+    ];
+}
