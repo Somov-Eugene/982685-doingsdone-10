e45df0c60e6621459c85d926d2eccbc24076fe35
Somov Eugene <somov.eugene.2012@gmail.com>
2019-09-21 16:22:57 +0300

    подключен vendor/autoload.php

--- init.php
+++ init.php
@@ -2,6 +2,7 @@
 date_default_timezone_set('Europe/Moscow');
 setlocale(LC_ALL, 'ru-RU');
 
+require_once 'vendor/autoload.php';
 require_once 'db_connect.php';
 require_once 'helpers.php';
 require_once 'database_functions.php';
