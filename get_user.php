6bdea64f25d3acc82427413a41c51cf9e2183369
Somov Eugene <somov.eugene.2012@gmail.com>
2019-09-24 20:43:08 +0300

    удален лишний код

--- get_user.php
+++ get_user.php
@@ -1,19 +1,8 @@
 <?php
 session_start();
 
-// получение данных пользователя из сессии
+$user = [];
+
 if (isset($_SESSION['user'])) {
-    $is_anonymous = false;
-
     $user = $_SESSION['user'];
-} else {
-    // в сессии нет данных - анонимный пользователь
-    $is_anonymous = true;
-
-    $user = [
-        'id' => '',
-        'name' => '',
-        'email' => '',
-        'password' => ''
-    ];
 }
