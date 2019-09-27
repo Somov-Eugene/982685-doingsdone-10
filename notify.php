95180694e2ab8aba0b867b5532176ee4b8c43bb2
Somov Eugene <somov.eugene.2012@gmail.com>
2019-09-27 23:36:21 +0300

    исправлен вызов функции, возращающей список задач на сегодня

--- notify.php
+++ notify.php
@@ -7,7 +7,7 @@ foreach ($send_users as $key => $recipient) {
     $msg_html = '<p><strong>Уважаемый(-ая) ' . $recipient['name'] . '!</strong></p>';
     $msg_html .= '<p>На ' . euro_date($recipient['date_completion']) . ' у Вас ';
 
-    $tasks = get_user_tasks_today($link, $recipient['id']);
+    $tasks = get_user_tasks($link, $recipient['id'], null, TASKS_FILTER_TODAY);
     $tasks_name = array_column($tasks, 'name');
 
     $msg_html .= (count($tasks_name) === 1) ? 'запланирована задача «' . $tasks_name[0] : 'запланированы задачи: «'. implode('», «', $tasks_name);
