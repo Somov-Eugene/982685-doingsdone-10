0061c11973495a33658d1cabf86e9f299034fe22
Somov Eugene <somov.eugene.2012@gmail.com>
2019-10-05 16:13:28 +0300

    параметры подключения к БД вынесены в отдельный конфигурационный файл

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,12 @@
 <?php
-$link = mysqli_connect('localhost', 'root', '', '982685-doingsdone-10');
+if (!file_exists("config/db.php")) {
+    echo 'Добавьте файл конфигурации подключения к БД: config/db.php';
+    exit;
+}
+
+require_once 'config/db.php';
+
+$link = mysqli_connect($database_host, $database_user, $database_password, $database_name);
 
 if (!$link) {
     $errorMsg = 'Ошибка подключения к БД. Дальнейшая работа сайта невозможна!';
