7ae70ec6562d295a3c8302a2e7d241b36bc0710a
Somov Eugene <somov.eugene.2012@gmail.com>
2019-10-05 21:48:09 +0300

    переделан сценарий: параметры SMTP берутся из конфига, сделана отдельная функция для получения списка незавершенных задач, упрощено формирование сообщения

--- notify.php
+++ notify.php
@@ -1,20 +1,22 @@
 <?php
 require_once 'init.php';
 
+$current_date = date('d.m.Y');
+
+// получаем список пользователей, имеющих задачи, срок выполнения которых истекает сегодня
 $send_users = get_users_tasks_expired_today($link);
 
 foreach ($send_users as $key => $recipient) {
-    $date = euro_date($recipient['date_completion']);
-
-    $tasks = get_user_tasks($link, $recipient['id'], null, TASKS_FILTER_TODAY);
+    // получаем список незавершенных задач на сегодня
+    $tasks = get_tasks_expired_today_by_user($link, $recipient['id']);
     $tasks_name = array_column($tasks, 'name');
-    $tasks_names = '«' . implode('», «', $tasks_name) . '»';
+    $tasks_list = '«' . implode('», «', $tasks_name) . '»';
 
     $msg_html = "
         <p>
             <strong>Уважаемый(-ая) {$recipient['name']}!</strong>
         </p>
-        <p>На {$date} у Вас запланированы следующие задачи: {$tasks_names}</p>
+        <p>На {$current_date} у Вас запланированы следующие задачи: {$tasks_list}</p>
     ";
 
     mail_sender($mailer, $from, $recipient['email'], $recipient['name'], $msg_html);
