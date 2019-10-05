d4ccecf8be22ef62651273ed4c6fe75f009d3404
Somov Eugene <somov.eugene.2012@gmail.com>
2019-10-05 16:52:25 +0300

    изменен состав и порядок подключения сценариев

--- init.php
+++ init.php
@@ -2,8 +2,8 @@
 date_default_timezone_set('Europe/Moscow');
 setlocale(LC_ALL, 'ru-RU');
 
-require_once 'vendor/autoload.php';
-require_once 'db_connect.php';
 require_once 'helpers.php';
+require_once 'db_connect.php';
 require_once 'database_functions.php';
+require_once 'mailer.php';
 require_once 'functions.php';
